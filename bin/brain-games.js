92f8cc8aac793e2fe873ea700fc45874a8876435
Andrew <slonryba@gmail.com>
2021-01-23 19:37:42 +0300

    divide the logic and exec file

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import { name } from '../src/cli.js';
 
 console.log("Welcome to the Brain Games!");
 
-const name = readlineSync.question('May I have your name? ');
-
-console.log(`Hello, ${name}!`);
+console.log(`Hello, ${name()}!`);
