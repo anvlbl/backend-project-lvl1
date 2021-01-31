b7c2c4948048384258cfded0f64803526190655c
Andrew <slonryba@gmail.com>
2021-01-31 13:24:33 +0300

    separate logic and exec of greeting

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const name = () => readlineSync.question('May I have your name? ');
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
 
-export { name };
+export { greeting };
