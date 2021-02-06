cd05485653bcc6dfe82bc27f6d299d35e269a4d8
Andrew <slonryba@gmail.com>
2021-02-06 15:36:46 +0300

    add new game Math prime

---
+++ src/brain-progression-logic.js
@@ -0,0 +1,24 @@
+import { gameLoop } from './index.js';
+
+const rule = 'What number is missing in the progression?';
+
+const setQuestion = () => {
+  const seqLength = Math.floor(Math.random() * 5) + 5;
+  const stepSize = Math.floor(Math.random() * 9) + 1;
+  const startValue = Math.floor(Math.random() * 60);
+  const sequence = [startValue];
+
+  for (let i = 0; i < seqLength; i += 1) {
+    const step = sequence[i] + stepSize;
+    sequence.push(step);
+  }
+
+  const hiddenIndex = Math.floor(Math.random() * seqLength);
+  const data = sequence.splice(hiddenIndex, 1, '..')[0];
+  const question = sequence.join(' ');
+  return { question, data };
+};
+
+const getResult = (data) => data;
+
+export default () => gameLoop(rule, setQuestion, getResult);
